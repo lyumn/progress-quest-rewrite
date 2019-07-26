43866fe90cd08c80178055c94f0741694b77fff8
lyumn <me@lyumn.net>
2019-07-26 08:55:00 -0400

    quest checkboxes

--- src/components/Quests.jsx
+++ src/components/Quests.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import { Box, Grid } from 'grommet';
+import { Box, Grid, CheckBox } from 'grommet';
 
 import BoxWithTitle from './shared/BoxWithTitle';
 import ProgressBar from './shared/ProgressBar';
 
-const Quests = props => (
-  <Grid
-    areas={[
-      { name: 'table', start: [0, 0], end: [0, 0] },
-      { name: 'tracking', start: [0, 1], end: [0, 1] }
-    ]}
-    columns={['flex']}
-    rows={['flex', '10px']}
-    gap="small"
-  >
-    <BoxWithTitle gridArea="table" title="Quests">
-      {JSON.stringify(props.data).toString()}
-    </BoxWithTitle>
-    <Box gridArea="tracking">
-      <ProgressBar width={props.questProgress} />
-    </Box>
-  </Grid>
-);
+const Quests = props => {
+  const array =
+    props.data.length > 10 ? props.data.slice(Math.max(props.data.length - 10, 1)) : props.data;
+
+  return (
+    <Grid
+      areas={[
+        { name: 'table', start: [0, 0], end: [0, 0] },
+        { name: 'tracking', start: [0, 1], end: [0, 1] }
+      ]}
+      columns={['flex']}
+      rows={['flex', '10px']}
+      gap="small"
+    >
+      <BoxWithTitle gridArea="table" title="Quests">
+        {array.map((e, i) => (
+          <CheckBox checked={i < array.length - 1} label={e} style={{ margin: '2px 0px' }} />
+        ))}
+      </BoxWithTitle>
+      <Box gridArea="tracking">
+        <ProgressBar width={props.questProgress} />
+      </Box>
+    </Grid>
+  );
+};
 
 // Inventory.propTypes = {
 //   data: PropTypes.arrayOf(PropTypes.shape({

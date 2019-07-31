0ab71a8d5014d70abcb488b7a30874a40d5df72d
lyumn <me@lyumn.net>
2019-07-31 07:32:53 -0400

    Updates

---
+++ precache-manifest.8b5a3098f3a312334170a52e193000e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c706cd86815d8f129203c98f15b1a11b",
+    "url": "/progress-quest-rewrite/index.html"
+  },
+  {
+    "revision": "f748fceda79a5a83fd02",
+    "url": "/progress-quest-rewrite/static/css/main.f1765acb.chunk.css"
+  },
+  {
+    "revision": "5dcae9a544e4ee4ef573",
+    "url": "/progress-quest-rewrite/static/js/2.ef1df4d3.chunk.js"
+  },
+  {
+    "revision": "f748fceda79a5a83fd02",
+    "url": "/progress-quest-rewrite/static/js/main.9305e143.chunk.js"
+  },
+  {
+    "revision": "7b69c6f0c015efb76211",
+    "url": "/progress-quest-rewrite/static/js/runtime~main.68c36208.js"
+  }
+]);
--- src/reducers/concerns/bars.js
+++
@@ -1,8 +0,0 @@
-export const humanizePosition = (position, total) => `${(position / total).toFixed(2) * 100}%`;
-
-export const increment = (state, value) => {
-  const newState = { ...state };
-  newState.position += value || 1;
-
-  return newState;
-};
